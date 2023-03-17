301f6be9ca808bc64e571afff0850d0d7ea65bf4
kirtiverma17 <110229852+kirtiverma17@users.noreply.github.com>
2023-03-17 21:06:03 +0530

    Update MainSection.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,26 +3,23 @@ import Inbox from "./Inbox";
 import Next7Days from "./Next7Days";
 import Today from "./Today";
 
-const list = [
-  { number: 1, title: "Let's complete this", date: new Date("9/27/2022") },
-  { number: 2, title: "Should sleep at 9pm", date: new Date("9/28/2022") },
-  { number: 3, title: "Should complete react", date: new Date("10/5/2022") }
-];
+
 
 const MainSection = (props) => {
-  const [filteredList, setFilteredList] = useState(list);
-  const addToList = (obj) => {
-    list.push(obj);
-    setFilteredList(list);
-  };
+  const mylist = [
+    { number: 1, title: "Let's complete this", date: new Date("9/27/2022") },
+    { number: 2, title: "Should sleep at 9pm", date: new Date("9/28/2022") },
+    { number: 3, title: "Should complete react", date: new Date("10/5/2022") },
+  ];
 
+  const [list, setList] = useState(mylist);
   return (
     <div className="main-section">
       {props.active === "INBOX" && (
-        <Inbox list={filteredList} append={addToList} />
+        <Inbox list={list} append={setList} />
       )}
-      {props.active === "TODAY" && <Today list={filteredList} />}
-      {props.active === "NEXT" && <Next7Days list={filteredList} />}
+      {props.active === "TODAY" && <Today list={list.filter(li => li.date.toLocaleDateString() == new Date().toLocaleDateString)} />}
+      {props.active === "NEXT" && <Next7Days list={list} />}
     </div>
   );
 };
